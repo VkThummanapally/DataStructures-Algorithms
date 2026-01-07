e852c99516d3fd5b96f9921b4430ea3081deeb7d
VkThummanapally <vineethkumar26t@gmail.com>
2026-01-07 22:19:44 +0530

    Move Zeros To End

---
+++ src/main/java/com/dsa/amazonprep/MoveZerosToEnd.java
@@ -0,0 +1,20 @@
+package com.dsa.amazonprep;
+
+import java.util.Arrays;
+
+public class MoveZerosToEnd {
+    public static void main(String args[]){
+        int[] arr = {0,1,0,3,12};
+        int index = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != 0){
+                arr[index++] = arr[i];
+            }
+        }
+        while(index < arr.length){
+            arr[index++] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
